471abe278c7b47d82ca1a389b344b57ccec91cbd
toyoce <toyo.naoki0701@gmail.com>
2022-10-23 18:06:26 +0900

    DBの接続設定を修正

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from resources.user import User, UserLogin, UserLogout, UserRegister
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 
